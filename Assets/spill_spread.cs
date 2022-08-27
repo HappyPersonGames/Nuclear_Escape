6b9c2250f5b1313d9b229616dd75a89aaaec4197
Oded Ginat <odedginat@gmail.com>
2022-08-28 00:28:22 +0300

    00:28

--- Assets/spill_spread.cs
+++ Assets/spill_spread.cs
@@ -6,26 +6,26 @@ using UnityEngine.Rendering.Universal;
 public class spill_spread : action
 {
     private bool start_spreading = false;
-    private float t;
+    private bool t = true;
     [SerializeField]
     private float growingBy = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        t = Time.fixedTime;
-    }
+    
+    public float i;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (start_spreading)
+        i = GetComponentInChildren<Light2D>().intensity;
+        if (start_spreading && t)
         {
+            t = false;
             growingBy *= 3;
         }
         transform.localScale += new Vector3(growingBy, growingBy, growingBy);
         GetComponentInChildren<Light2D>().pointLightOuterRadius += growingBy;
-        GetComponentInChildren<Light2D>().pointLightInnerRadius += growingBy;
-        GetComponentInChildren<Light2D>().intensity += growingBy;
+        // GetComponentInChildren<Light2D>().pointLightInnerRadius += growingBy;
+        if(i < 1.5)
+            GetComponentInChildren<Light2D>().intensity += growingBy;
         // t = Time.fixedTime;
         
     }
